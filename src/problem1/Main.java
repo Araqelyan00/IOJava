e815643dded8234d8838b80968f5a054a6fbe13e
sdfsdf <aramaraqelyan503@gmail.com>
2022-04-05 12:15:45 +0400

    changed!

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -6,18 +6,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         File file = new File("Hello.txt");
+        File outputFile = new File("OutputFile.txt");
         if (!file.exists()) {
             file.createNewFile();
         }
         FileWriter fileWriter = new FileWriter(file);
+
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write("Hello world, hello darkness my old friend hello hello");
+        bufferedWriter.write("Hello world, hello darkness my old friend hello hello\n" +
+                "Hello darling.\n" +
+                "Hello hello");
         bufferedWriter.flush();
         bufferedWriter.close();
+        BufferedWriter bf = new BufferedWriter(new FileWriter(outputFile));
 
             String line;
             int count = 0;
-            int numOfHelloWords = 0;
+
 
             //Opens a file in read mode
             FileReader fileReader = new FileReader(file);
@@ -25,20 +30,24 @@ public class Main {
 
             //Gets each line till end of file is reached
             while((line = br.readLine()) != null) {
-                //Splits each line into words
+                int numOfHelloWords = 0;
+                count ++;
                 String[] words;
                 words = line.split(" ");
-                //Counts each word
-                count = count + words.length;
                 for (String s: words){
                     if (s.equals("hello") || s.equals("Hello")){
                         numOfHelloWords++;
                     }
                 }
+
+                bf.write("In line : " + count +" - " + numOfHelloWords);
+                bf.newLine();
+                //System.out.println("In line" + count +" - " + numOfHelloWords);
+
             }
 
-
-            System.out.println("Number of hello in given file: " + numOfHelloWords);
+            bf.flush();
+            bf.close();
             br.close();
         }
 }
